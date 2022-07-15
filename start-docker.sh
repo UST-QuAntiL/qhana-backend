23105e26c0f262ad642424451e6a1782a49bc42d
krebsni <st151522@stud.uni-stuttgart.de>
2022-07-15 12:19:02 +0200

    inline liquibase properties, revert overwrite, don't copy changelog

--- start-docker.sh
+++ start-docker.sh
@@ -5,10 +5,19 @@ cp /app/liquibase.properties /app/data/liquibase.properties
 liquibase --url=jdbc:sqlite:qhana-backend.db --classpath=../liquibase/internal/lib/sqlite-jdbc.jar --driver=org.sqlite.JDBC --changelog-file=../changelog.xml updateTestingRollback
 
 # insert env var for mariadb liquibase config
-eval "echo \"$(cat /app/liquibase.mariadb.properties)\"" > /app/data/liquibase.properties
+cat << EOF > /app/data/liquibase.properties
+classpath: ../liquibase/internal/lib/mariadb-java-client.jar
+url: jdbc:$QHANA_DB_TYPE://$QHANA_DB_HOST/$QHANA_DB_NAME
+changelog-file: ../changelog.xml
+username: $QHANA_DB_USER
+password: $QHANA_DB_PASSWORD
+
+liquibase.secureParsing=false
+liquibase.hub.mode=off
+EOF
 
 # copy config and qhana db into volume
-cp /app/Config.toml /app/changelog.xml /app/data/
+cp -n /app/Config.toml /app/data/
 
 
 # wait for db to start
